fc319dfc56f1c47ec6f8203ab3b8e27700144639
vincenzoarena <penninoematita@gmail.com>
2022-03-16 13:50:59 +0100

    2022-03-16 - JavaScript Day 00001

---
+++ mar/16-03/workspace/02-variables.js
@@ -0,0 +1,33 @@
+// the naming convention in JavaScript is camelCase
+
+//variable declaration (or variable definition)
+let myVariable;
+
+console.log(myVariable);
+
+// let's assign a value to the variable
+
+
+// assignment
+myVariable = 28;
+
+console.log(myVariable);
+
+// let's declare a variable and initialize it to a value
+// all in one line
+
+let className = "E11";
+
+console.log(className);
+
+
+let sum = 2 + 4 + 5 - 9;
+
+let number = 2;
+number = 2 + 2;
+
+console.log(number);
+
+result = number + 1
+
+console.log(result);
---
+++ mar/16-03/workspace/04-console.js
@@ -0,0 +1,19 @@
+/**
+ * We can use console.log() to output anything we want to the console
+ * 
+ */
+
+// we can also output more than 1 value
+// or more than 1 variable at the same time
+// simply separate them by a comma
+
+console.log("hello", 1, 2, 3, true, false);
+
+console.log("hello", "world");
+
+let firstName = "Carlo"
+let city = "Berlin"
+
+console.log(firstName);
+
+console.log(city);
