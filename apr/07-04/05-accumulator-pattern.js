8d6b702adcae702877eb7c8773d9753c88eff2a5
Carlo Trimarchi <26264+carlotrimarchi@users.noreply.github.com>
2022-04-07 14:37:51 +0200

    lesson 2022-04-07

---
+++ apr/07-04/05-accumulator-pattern.js
@@ -0,0 +1,12 @@
+/**
+ * Accumulator pattern
+ * 
+ * - we have a variable (an accumulator) initialized to some value, normally:
+ *  - 0, if we want a number as result
+ *  - "", if we want a string as result
+ *  - [], if we want an array as result
+ * - we have a loop that goes through a string, an array, 
+ * or simply generates list of numbers
+ * - we fill the accumulator variable from withing the loop
+ * - we return the accumulator variable
+ */
